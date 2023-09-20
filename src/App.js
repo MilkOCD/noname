702f8b8a366570aa7125d4ffd146ba5180cc857d
minhchien <milkocd@gmail.com>
2023-09-20 11:43:55 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import classnames from 'classnames/bind';
+import styles from './App.module.scss';
+
+const cx = classnames.bind(styles);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Road />
     </div>
   );
+}
+
+const Road = () => {
+  const [step, toStep] = useState(0);
+  const [score, newScore] = useState(0);
+
+  return <div className={cx('road-container')}>
+    <div className={cx('div-note')}></div>
+    <div className={cx('grid-emoji')}>
+      <div className={cx('emoji-item-odd')}>
+        <img alt='' src="/images/emojis/1.png"/>
+        <div className={cx('emoji-desc')}>name</div>
+      </div>
+      <div className={cx('emoji-item')}>
+        <img alt='' src="/images/emojis/2.png"/>
+        <div className={cx('emoji-desc')}>name</div>
+      </div>
+      <div className={cx('emoji-item-odd')}>
+        <img alt='' src="/images/emojis/3.png"/>
+        <div className={cx('emoji-desc')}>name</div>
+      </div>
+      <div className={cx('emoji-item')}>
+        <img alt='' src="/images/emojis/4.png"/>
+        <div className={cx('emoji-desc')}>name</div>
+      </div>
+      <div className={cx('emoji-item-odd')}>
+        <img alt='' src="/images/emojis/5.png"/>
+        <div className={cx('emoji-desc')}>name</div>
+      </div>
+      <div className={cx('emoji-item')}>
+        <img alt='' src="/images/emojis/6.png"/>
+        <div className={cx('emoji-desc')}>name</div>
+      </div>
+      <div className={cx('emoji-item-odd')}>
+        <img alt='' src="/images/emojis/7.png"/>
+        <div className={cx('emoji-desc')}>name</div>
+      </div>
+      <div className={cx('emoji-item')}>
+        <img alt='' src="/images/emojis/8.png"/>
+        <div className={cx('emoji-desc')}>name</div>
+      </div>
+      <div className={cx('emoji-item-odd')}>
+        <img alt='' src="/images/emojis/9.png"/>
+        <div className={cx('emoji-desc')}>name</div>
+      </div>
+    </div>
+  </div>
 }
 
 export default App;
